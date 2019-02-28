7aa9e8efc2979e1daefb466133cade7803b13a65
Dmitriy Ivanov <Dmitriy.ivanov@noveogroup.com>
2019-02-28 18:30:39 +0300

    Clean redundant code and comments

--- CarGoesToTap/ViewController.swift
+++ CarGoesToTap/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  CarGoesToTap
-//
-//  Created by Dmitry on 26/02/2019.
-//  Copyright © 2019 Dmitry. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
